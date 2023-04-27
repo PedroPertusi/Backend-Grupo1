abf8bbcc5bb08e961941e346765351eaf0718310
alfredjynx <99195331+alfredjynx@users.noreply.github.com>
2023-04-27 13:46:24 -0300

    retirada de Entity do DTO

--- motorista/src/main/java/com/crud/motorista/motorista/DTO/MotoristaReturnDTO.java
+++ motorista/src/main/java/com/crud/motorista/motorista/DTO/MotoristaReturnDTO.java
@@ -1,23 +1,18 @@
 package com.crud.motorista.motorista.DTO;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
 @AllArgsConstructor
 public class MotoristaReturnDTO {
 
-    @Column
     private String name;
 
-    @Column
     private String placa;
 
-    @Column
     private String modelo;
     
 }
--- motorista/src/main/java/com/crud/motorista/motorista/DTO/MotoristaSaveDTO.java
+++ motorista/src/main/java/com/crud/motorista/motorista/DTO/MotoristaSaveDTO.java
@@ -1,42 +1,27 @@
 package com.crud.motorista.motorista.DTO;
 
-import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
 @Setter
-@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class MotoristaSaveDTO {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
-    @Column
     @NonNull
     private String name;
 
-    @Column
     @NonNull
     private Integer cpf;
 
-    @Column
-    @NonNull
     private String placa;
 
-    @Column
-    @NonNull
     private String modelo;
 
-    @Column
     private Double precoViagem;
 
-    @Column
     private String status;
 
-    @Column
     private String ocupacao; 
     
 }
