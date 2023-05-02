0b806b3af635dd0713a7cd16b28f2262c2b1907e
PedroPertusi <pedrovazpertusi@gmail.com>
2023-05-02 11:16:14 -0300

    controller validaMotorista, motoristaDisponível, liberaMotorista

--- motorista/src/main/java/com/crud/motorista/motorista/MotoristaController.java
+++ motorista/src/main/java/com/crud/motorista/motorista/MotoristaController.java
@@ -1,5 +1,37 @@
 package com.crud.motorista.motorista;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.crud.motorista.motorista.DTO.MotoristaReturnDTO;
+
+@RestController
+@RequestMapping("/motorista")
 public class MotoristaController {
+    @Autowired
+    private MotoristaService motoristaService;
+
+
     
+    // Rota que valida o motorista (status LIBERADO para fazer corridas)
+    @GetMapping("/{identifier}")
+    public MotoristaReturnDTO validaMotorista(@PathVariable String identifier) {
+        return motoristaService.validaMotorista(identifier);
+    }
+
+    // Rota que retorna um motorista disponível aleatório
+    @GetMapping
+    public MotoristaReturnDTO motoristaDisponivel() {
+        return motoristaService.motoristaDisponivel();
+    }
+
+    // Rota que altera o motorista de indisponível p/ disponível 
+    @PostMapping("/available/{identifier}")
+    public MotoristaReturnDTO liberaMotorista (@PathVariable String identifier) {
+        return motoristaService.liberaMotorista(identifier);
+    }
 }
